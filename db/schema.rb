2a1a09ea46afecdfc9498ba411fe0d2e348ccc2b
Nevetha Prakash <nevethap@thoughtworks.com>
2016-04-07 18:28:19 +0530

    added age to doctors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407124033) do
+ActiveRecord::Schema.define(version: 20160407125635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160407124033) do
     t.string   "specialization"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "age"
   end
 
   create_table "posts", force: :cascade do |t|
